f9ae879bf2923b38c4bed224125aded73f75e79c
Danny Willems <be.danny.willems@gmail.com>
2021-06-17 12:15:42 +0200

    Implement get_nth_root_of_unity for Fr with pre-defined roots

--- src/gen/fr.ml
+++ src/gen/fr.ml
@@ -25,8 +25,14 @@
 module type T = sig
   include Ff_sig.PRIME
 
-  (** Check if a point, represented as a byte array, is in the field **)
+  (** Check if a point, represented as a byte array, is in the field *)
   val check_bytes : Bytes.t -> bool
+
+  (** Returns a 2-nth root of unity.
+      The values are generated from a list of pre-defined roots of unity.
+      Accepted values are between 2 and 32.
+  *)
+  val get_nth_root_of_unity : int -> t
 
   val fft : domain:t array -> points:t list -> t list
 
@@ -110,6 +116,35 @@ module MakeFr (Stubs : S.RAW_BASE) : T = struct
             aux m c t r
         in
         Some (aux s c (pow x q) (pow x (Z.divexact (Z.succ q) two_z)))
+
+  let get_nth_root_of_unity i =
+    let g_2_32 =
+      of_string
+        "21584124886548760190346392867028830688912556631271990304491841940743921295609"
+    in
+
+    let g_2_16 =
+      of_string
+        "45578933624873246016802258050230213493140367389966312656957679049059636081617"
+    in
+
+    let g_2_8 =
+      of_string
+        "27611812781829920551290133267575249478648871281233506899293410857719571783635"
+    in
+
+    let g_2_4 =
+      of_string
+        "16624801632831727463500847948913128838752380757508923660793891075002624508302"
+    in
+    let g_2_2 = pow g_2_4 (Z.of_int 4) in
+    let ( - ) = Int.sub in
+    if i > 32 || i < 1 then failwith "Supported values are between 2 and 32"
+    else if i > 16 then pow g_2_32 (Z.of_int (1 lsl (32 - i)))
+    else if i > 8 then pow g_2_16 (Z.of_int (1 lsl (16 - i)))
+    else if i > 4 then pow g_2_8 (Z.of_int (1 lsl (8 - i)))
+    else if i > 2 then pow g_2_4 (Z.of_int (1 lsl (4 - i)))
+    else g_2_2
 
   let fft ~domain ~points =
     let module M = struct
