ad962f87de4873e63220f5d0079355c134faa0cb
Danny Willems <be.danny.willems@gmail.com>
2021-06-29 21:14:28 +0200

    Improve benches

---
+++ benchmark/bench_fr.ml
@@ -0,0 +1,55 @@
+open Core_bench
+
+module MakeBenchBlst = struct
+  module F = Bls12_381.Fr
+
+  let test_addition =
+    let e1 = F.random () in
+    let e2 = F.random () in
+    Bench.Test.create ~name:"Addition Fr" (fun () -> ignore (F.add e1 e2))
+
+  let test_multiplication =
+    let e1 = F.random () in
+    let e2 = F.random () in
+    Bench.Test.create ~name:"Multiplication Fr" (fun () -> ignore (F.mul e1 e2))
+
+  let test_sub =
+    let e1 = F.random () in
+    let e2 = F.random () in
+    Bench.Test.create ~name:"Substraction Fr" (fun () -> ignore (F.sub e1 e2))
+
+  let test_negate =
+    let e1 = F.random () in
+    Bench.Test.create ~name:"Opposite Fr" (fun () -> ignore (F.negate e1))
+
+  let test_square =
+    let e1 = F.random () in
+    Bench.Test.create ~name:"Square Fr" (fun () -> ignore (F.square e1))
+
+  let test_double =
+    let e1 = F.random () in
+    Bench.Test.create ~name:"Double Fr" (fun () -> ignore (F.double e1))
+
+  let test_inverse =
+    let e1 = F.random () in
+    Bench.Test.create ~name:"Inverse Fr" (fun () -> ignore (F.inverse_exn e1))
+
+  let test_pow =
+    let e1 = F.random () in
+    let n = F.(to_z (random ())) in
+    Bench.Test.create ~name:"Pow Fr" (fun () -> ignore (F.pow e1 n))
+
+  let get_benches () =
+    [ test_addition;
+      test_multiplication;
+      test_negate;
+      test_sub;
+      test_square;
+      test_inverse;
+      test_pow;
+      test_double ]
+end
+
+let () =
+  let commands = List.concat [MakeBenchBlst.get_benches ()] in
+  Core.Command.run (Core_bench.Bench.make_command commands)
