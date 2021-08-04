f4da495c8d8339de169c1777e87daea613147938
Danny Willems <be.danny.willems@gmail.com>
2021-08-04 19:12:13 +0200

    Blst/Interface: documentation

--- src/virtual/bls12_381.mli
+++ src/virtual/bls12_381.mli
@@ -52,23 +52,43 @@ end
 
 (** Follow https://tools.ietf.org/pdf/draft-irtf-cfrg-bls-signature-04.pdf *)
 module Signature : sig
+  (** Type of the secret keys. *)
   type sk
 
+  (** Type of the public keys *)
   type pk
 
   (* Not abstracting the type to avoid to write (de)serialisation routines *)
   type signature = Bytes.t
 
+  (** [sk_of_bytes_exn bs] attempts to deserialize [bs] into a secret key. [bs]
+      must be the little endian representation of the secret key.
+      In this case, secret keys are scalars of BLS12-381 and are encoded on 32
+      bytes. The bytes sequence might be less of 32 bytes and in this case, the
+      bytes sequence is padded on the right by 0's. If the bytes sequence is
+      longer than 32 bytes, raise [Invalid_argument].
+  *)
   val sk_of_bytes_exn : Bytes.t -> sk
 
+  (** [sk_to_bytes sk] serialises the secret key into the little endian
+      representation.
+  *)
   val sk_to_bytes : sk -> Bytes.t
 
+  (** [generate_sk ?key_info ikm] generates a new (random) secret key. [ikm]
+       must be at least 32 bytes (otherwise, raise [Invalid_argument]). The
+       default value of [key_info] is the empty bytes sequence.
+  *)
   val generate_sk : ?key_info:Bytes.t -> Bytes.t -> sk
 
+  (** [derive_pk sk] derives the corresponding public key of [sk]. *)
   val derive_pk : sk -> pk
 
+  (** [aggregate_signature_opt signatures] aggregates the signatures [signatures], following
+      https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.8.
+      Return [None] if [INVALID] is expected in the specification
+  *)
   val aggregate_signature_opt : Bytes.t list -> Bytes.t option
-
 
   (**
     https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-3.1
@@ -85,6 +105,7 @@ module Signature : sig
 
     val verify : pk -> Bytes.t -> signature -> bool
 
+    (** raise [Invalid_argument] if the messages are not distinct *)
     val aggregate_verify : (pk * Bytes.t) list -> signature -> bool
   end
 
@@ -103,17 +124,44 @@ module Signature : sig
     val aggregate_verify : (pk * Bytes.t) list -> signature -> bool
   end
 
+  (**
+     https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-3.3
 
+     A proof of possession scheme uses a separate public key validation
+     step, called a proof of possession, to defend against rogue key
+     attacks.  This enables an optimization to aggregate signature
+     verification for the case that all signatures are on the same
+     message.
+  *)
   module Pop : sig
     (* Not abstracting the type to avoid to write (de)serialisation routines *)
     type proof = Bytes.t
 
+    (** Equivalent to [core_sign] with the DST given in the specification
+        https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-4.2.3
+    *)
     val sign : sk -> Bytes.t -> signature
 
+    (** Equivalent to [core_verify] with the DST given in the specification
+        https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-4.2.3
+    *)
     val verify : pk -> Bytes.t -> signature -> bool
 
+    (** [pop_proof sk] implements
+         https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-3.3.2
+    *)
     val pop_prove : sk -> proof
+
+    (** [pop_verify pk signature] implements
+        https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-3.3.3
+    *)
     val pop_verify : pk -> proof -> bool
+
+    (**
+      [aggregate_verify pks msg aggregated_signature] performs a aggregate
+      signature verification. It supposes the same message [msg] has been
+      signed.
+    *)
     val aggregate_verify : (pk * proof) list -> Bytes.t -> signature -> bool
   end
 end
